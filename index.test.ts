e448ad7d4bedc92dacadecfed1d78a394b0e777e
Levin Keller <post@levinkeller.de>
2023-07-16 18:42:24 +0200

    chore: test with mock-jwks@3

--- index.test.ts
+++ index.test.ts
@@ -1,23 +1,25 @@
-import { assert } from 'chai'
-import {createApp} from "./index";
+import { assert } from "chai";
+import { createApp } from "./index.js";
 import createJWKSMock from "mock-jwks";
-import request from 'supertest'
+import request from "supertest";
+import { describe, test } from "vitest";
+describe("Test API auth", () => {
+  test("Api auth", async () => {
+    const jwksMock = createJWKSMock("https://example.org");
+    const app = createApp({
+      jwksUri: "https://example.org/.well-known/jwks.json",
+    });
 
-describe('Test API auth', () => {
-    test('Api auth', async () => {
-        const jwksMock = createJWKSMock('https://example.org/')
-        const app = createApp({
-            jwksUri: 'https://example.org/.well-known/jwks.json'
-        })
+    const accessToken = jwksMock.token({
+      aud: "private",
+      iss: "master",
+    });
 
-        const accessToken = jwksMock.token({
-            aud: 'private',
-            iss: 'master',
-        })
-
-        jwksMock.start()
-        const response = await request(app).get('/').set('Authorization', `Bearer ${accessToken}`)
-        assert.equal(response.status,  200)
-        await jwksMock.stop()
-    })
-})
+    jwksMock.start();
+    const response = await request(app)
+      .get("/")
+      .set("Authorization", `Bearer ${accessToken}`);
+    assert.equal(response.status, 200);
+    await jwksMock.stop();
+  });
+});
